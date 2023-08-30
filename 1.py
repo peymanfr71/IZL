86917a20303e76d0167e1f3279a35c9452b8d47f
Peyman <143264765+peymanfr71@users.noreply.github.com>
2023-08-30 10:21:48 +0330

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,22 @@
+my_list = [45, 87.5, 32, 15, 7]
+total_sum = sum(my_list)
+print("Sum of all items:", total_sum)
+print (my_list.sort())
+
+my_list1 = [10, 4, 5, 34, 23]
+my_list.sort()
+print (my_list)
+maximum = max(my_list)
+print ("the largest number is: ", maximum)
+
+my_list = [45, 87.5, 32, 15, 7]
+total = sum(my_list)
+count = len(my_list)
+average = total / count
+print (total, count, average)
+
+my_list1 = [10, 4, 5, 34, 23]
+total = sum(my_list1)
+count = len(my_list1)
+average = total / count
+print("The average is:", average)
