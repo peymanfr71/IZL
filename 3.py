2191aca47fc857fdab397fe7ae90ca382e5359a7
Peyman <143264765+peymanfr71@users.noreply.github.com>
2023-08-30 10:24:32 +0330

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,13 @@
+age = input ("Please Enter your age: ")
+print ("Great! you have", age , "yaers old!")
+# ##
+birthday = int(input("Please Enter your birthday year: "))
+current_date = int(input("Please Enter current date: "))
+age = current_date - birthday
+print ("your age is:", age)
+# ##
+print (int("1995"))
+print (type(int("1995")))
+# ##
+age = 2022 - int('1995')
+print (age, type(age))
