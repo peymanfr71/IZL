f284f06efa0e9334511cf27853de744356bb356e
Peyman <143264765+peymanfr71@users.noreply.github.com>
2023-08-30 10:23:08 +0330

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,12 @@
+my_giant_list = [10, 5, 125, -14, -45, 2, 0, 250, 7, 66, 4, 12, 11, -23, -5]
+
+# start here:
+sumi = sum(my_giant_list)
+maxi = max(my_giant_list)
+mini = min(my_giant_list)
+print (sumi, maxi, mini)
+
+# start here:
+print (sum(my_giant_list))
+print (max(my_giant_list))
+print (min(my_giant_list))
